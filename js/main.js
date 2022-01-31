fab085e84752d793b5ce333693af4fdc6bc5be72
Polina <88159970+PolinaTimofeeva@users.noreply.github.com>
2022-01-31 13:23:42 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function() {
+    
+    $('#tags__toggle').on('click', function (e) {
+    const btnLabel = $('.tags-hidden')
+        .toggle()
+        .is(':visible')
+        ? 'Свернуть' : 'Показать еще';
+    $(e.target).text(btnLabel);
+    });
+
+    $('.main__item-arrow').click(function(event){
+        $(this).toggleClass('active').next().slideToggle(300);
+    });
+
+    $(function(){
+        $('#slider').slider({
+            range: true,
+            min: 0,
+            max: 50000,
+            values: ['0', '100000'],
+            slide: function(event, ui) {
+                $('#min').val(ui.values[0]);
+                $('#max').val(ui.values[1]);
+            }
+        });
+    });
+
+ 
+})
+
+
+
